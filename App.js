5e460314897d95c232c80de04c7b3c5aa06276ec
Tarık Şişbacak <tariksisbacak@gmail.com>
2023-11-20 02:24:56 +0300

    React Native Tailwindcss with Nativewind and Darkmode

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Pressable, Switch, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {styled, useColorScheme} from "nativewind";
+
+const StyledPressable = styled(Pressable)
+const StyledText = styled(Text)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const {colorScheme, toggleColorScheme} = useColorScheme();
+
+    return (
+        <View
+            className="flex-1 items-center justify-center bg-blue-300 dark:bg-black"
+        >
+            <View
+                selectable={false}
+                className="flex flex-col items-center dark:text-white"
+            >
+                <Switch value={colorScheme === "dark"} onChange={toggleColorScheme}/>
+                <View className="mt-2 dark:text-white"><Text
+                    className="dark:text-white mt-2">{`Try clicking me! ${colorScheme === "dark" ? "🌙" : "🌞"}`}</Text></View>
+            </View>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
